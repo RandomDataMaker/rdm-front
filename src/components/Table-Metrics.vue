acd1648fed9f7de60ce48c222847f734ee6a76ab
SaronTetra <starwars.maciek@gmail.com>
2019-12-20 05:33:45 +0100

    Added: Attributes display under metrics

--- src/components/Table-Metrics.vue
+++ src/components/Table-Metrics.vue
@@ -2,17 +2,18 @@
     <div>
         <b-table
                 :data="metrics.filter(metric => metric.patient_id == patientId)"
+                detailed
         >
             <template slot-scope="props">
-                <b-table-column field="id" label="ID" numeric>
+                <b-table-column field="id" label="ID" width="80" numeric >
                     {{props.row.id}}
                 </b-table-column>
 
-                <b-table-column field="patient_id" label="Patient ID" numeric>
+                <b-table-column field="patient_id" label="Patient ID" width="80" numeric>
                     {{props.row.patient_id}}
                 </b-table-column>
 
-                <b-table-column field="created" label="Created">
+                <b-table-column field="created" label="Created" width="240">
                     {{props.row.created.substring(0,10)}}
                 </b-table-column>
 
@@ -23,6 +24,15 @@
                 <b-table-column field="attributes" label="Attributes">
                     {{props.row.attributes}}
                 </b-table-column>
+            </template>
+            <template slot="detail" slot-scope="props">
+                <ul>
+                    <li v-for="(value, key) in JSON.parse(props.row.attributes)" :key="key">
+                        <b>{{key}}</b>:
+                        {{value}}
+<!--                        {{attribute.key}} {{attribute.value}}-->
+                    </li>
+                </ul>
             </template>
         </b-table>
     </div>
