863130106e6c5ac06a790492b6f48149ec11dba1
SaronTetra <starwars.maciek@gmail.com>
2019-12-20 02:52:20 +0100

    Repo init

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <div id="app">
+        <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+        <div id="header"></div>
+<!--        <div class="face">-->
+<!--            <img :src="image" :width="size">-->
+<!--        </div>-->
+<!--        <div class="face-info">-->
+<!--            {{ faceinfo }}-->
+<!--            <a :href="person_details">MORE</a>-->
+<!--            <p v-if="alive">Żyje</p>-->
+<!--            <p v-else>Umarł</p>-->
+<!--            <button @click="clickTest">Click</button>-->
+<!--        </div>-->
+       <table-people></table-people>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import TablePeople from "./components/Table-People";
+    // import VueFluent from 'vue-fluent';
+    // import 'vue-fluent/dist/vue-fluent.min.css';
+    // Vue.use(VueFluent)
+    import Buefy from 'buefy';
+    import 'buefy/dist/buefy.css'
+    Vue.use(Buefy)
+
+    export default {
+        name: 'app',
+        components: {TablePeople},
+        data() {
+            return {
+                product: 'It\'s working!',
+                faceinfo: "Tom Tester",
+                image: require("@/assets/skeleton.png"),
+                size: "100px",
+                person_details: "http://putelita.pl",
+                alive: true
+            }
+        },
+        methods: {
+            clickTest() {
+                this.alive = !this.alive
+            }
+        }
+    }
+
+</script>
+
+<style>
+    body{
+        margin: 0;
+        padding: 0;
+    }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+
+    }
+
+    #header {
+        margin-bottom: 100px;
+        height: 100px;
+        background-color: hsl(120, 100%, 50%);
+        background: linear-gradient(90deg, hsl(220, 70%, 50%), hsl(260, 50%, 50%));
+    }
+</style>
---
+++ src/components/Table-People.vue
@@ -0,0 +1,103 @@
+<template>
+   <div>
+       <section>
+           <b-table
+                   :data="data"
+                   :columns="columns"
+                   :paginated="true"
+                   :per-page="10"
+           >
+               <template slot-scope="props">
+                   <b-table-column field="id" label="ID" width="80" sortable numeric>
+                       {{ props.row.id }}
+                   </b-table-column>
+
+                   <b-table-column field="name" label="First Name" sortable searchable>
+                    {{ props.row.name}}
+                   </b-table-column>
+
+                   <b-table-column field="surname" label="Last Name" sortable searchable>
+                       {{ props.row.surname}}
+                   </b-table-column>
+
+                   <b-table-column field="birthday" label="Birthday" sortable searchable >
+                       {{ props.row.birthday}}
+                   </b-table-column>
+
+                   <b-table-column field="sex" label="Gender" width="140" centered sortable numeric>
+                    <span>
+                        <b-icon pack="fas"
+                                :icon="props.row.sex == 1 ? 'mars' : 'venus'">
+                        </b-icon>
+                    </span>
+                   </b-table-column>
+
+                   <b-table-column field="pesel" label="PESEL" sortable searchable>
+                       {{ props.row.pesel}}
+                   </b-table-column>
+               </template>
+           </b-table>
+       </section>
+   </div>
+<!--   <p> {{info}}</p>-->
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: "table-people",
+        data() {
+          return {
+              data: [],
+              columns: [
+                  {
+                      field: 'id',
+                      label: "ID",
+                      width: '80',
+                      numeric: true,
+                      sortable: true,
+                  },
+                  {
+                      field: 'name',
+                      label: 'First Name',
+                      searchable: true,
+                      sortable: true,
+                  },
+                  {
+                      field: 'surname',
+                      label: 'Last Name',
+                      searchable: true,
+                      sortable: true,
+                  },
+                  {
+                      field: 'birthday',
+                      label: 'Date',
+                      sortable: true,
+                      searchable: true,
+                      centered: true,
+                  },
+                  {
+                      field: 'sex',
+                      label: 'Gender',
+                      width: '120',
+                      centered: true,
+                      sortable: true,
+                  },
+                  {
+                      field: 'pesel',
+                      label: 'PESEL',
+                      searchable: true,
+                      sortable: true,
+                  }
+              ]
+          }
+        },
+        mounted() {
+            axios.get('http://localhost:8000/person/').then(response => (this.data = response.data))
+        }
+    }
+</script>
+
+<style scoped>
+</style>
